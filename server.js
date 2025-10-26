6a4bde8e66f81b34dab0dd17dcb65f92b1ce5627
Shrigulia <122960250+Shrigulia@users.noreply.github.com>
2025-10-26 23:37:40 +0530

    Refactor socket.io integration and event handling

--- server.js
+++ server.js
@@ -1,50 +1,37 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-
+const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
-    cors: {
-        origin: '*', // Allow from anywhere, or specify your Netlify URL
-        methods: ['GET', 'POST']
-    }
+const io = new Server(server, {
+    cors: { origin: '*', methods: ['GET','POST'] }
 });
+
+// app.use(express.static(__dirname)); // serve html, css, js
 
 const rooms = {};
 
-io.on('connection', (socket) => {
-    socket.on('join', (room) => {
+io.on('connection', socket => {
+
+    socket.on('join', room => {
         socket.join(room);
         if (!rooms[room]) rooms[room] = [];
         rooms[room].push(socket.id);
         io.to(room).emit('joined', { users: rooms[room].length });
     });
 
-    socket.on('movie-event', (data) => {
-        socket.to(data.room).emit('movie-event', data);
-    });
+    socket.on('movie-event', data => socket.to(data.room).emit('movie-event', data));
 
-    socket.on('offer', (data) => {
-        socket.to(data.room).emit('offer', data);
-    });
+    socket.on('chat', data => io.to(data.room).emit('chat', data));
 
-    socket.on('answer', (data) => {
-        socket.to(data.room).emit('answer', data);
-    });
-
-    socket.on('ice-candidate', (data) => {
-        socket.to(data.room).emit('ice-candidate', data);
-    });
-
-    socket.on('chat', (data) => {
-        io.to(data.room).emit('chat', data);
-    });
+    socket.on('offer', data => socket.to(data.room).emit('offer', data));
+    socket.on('answer', data => socket.to(data.room).emit('answer', data));
+    socket.on('ice-candidate', data => socket.to(data.room).emit('ice-candidate', data));
 
     socket.on('disconnect', () => {
         for (let room in rooms) {
             rooms[room] = rooms[room].filter(id => id !== socket.id);
-            if (rooms[room].length === 0) delete rooms[room];
+            if (rooms[room].length===0) delete rooms[room];
         }
     });
 });
